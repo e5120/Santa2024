a5e2ff0bac5d1c6386d2a5ddf93e2365c1e3af27
ShimazuSho <shimazu.sho.jri@outlook.jp>
2025-01-23 14:32:56 +0900

    add: Samplerの追加

--- santa/sampler.py
+++ santa/sampler.py
@@ -7,3 +7,15 @@ class UniformSampler(object):
 
     def sample(self):
         return random.choice(self.ops)
+
+
+class OrderSampler(object):
+    def __init__(self, ops):
+        self.ops = ops
+        self.n_ops = len(ops)
+        self.index = 0
+
+    def sample(self):
+        op = self.ops[self.index]
+        self.index = (self.index + 1) % self.n_ops
+        return op
